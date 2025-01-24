72cd391e69b052b2755951416916d0b6d940bff4
Etienne Donneger <etienne@pinax.network>
2025-01-24 15:13:29 +0000

    Update docker-entrypoint.sh
    - Replace `${NETWORK}`, `${NETWORK_URL}`, `${S3_BUCKET}`, `${S3_REGION}` and `${OUTPUT_STORE}` with full URLs variable `${SUBSTREAMS_ENDPOINT}` and `${OUTPUT_STORE_PATH}`
    - Add check for `${SUBSTREAMS_ENDPOINT}` and `${OUTPUT_STORE_PATH}` existence

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,14 +5,19 @@ set -e
 : "${ENCODER:=parquet}"
 : "${PARQUET_DEFAULT_COLUMN_COMPRESSION:=snappy}"
 : "${FILE_BLOCK_COUNT:=10000}"
-: "${S3_REGION:=us-east-1}"
 : "${STATE_STORE:=state.yaml}"
 : "${PRODUCTION_MODE:=false}"
 : "${METRICS_LISTEN_PORT:=9102}"
 
-OUTPUT_STORE="s3://pinax/${NETWORK}/${S3_BUCKET}?region=${S3_REGION}"
-NETWORK_URL="${NETWORK}.substreams.pinax.network:443"
 METRICS_LISTEN_ADDR="0.0.0.0:${METRICS_LISTEN_PORT}"
+
+if [[ -z "${SUBSTREAMS_ENDPOINT}" ]]; then
+  echo "Error: missing substreams endpoint (SUBSTREAMS_ENDPOINT)"
+fi
+
+if [[ -z "${OUTPUT_STORE_PATH}" ]]; then
+  echo "Error: missing s3 output store path (OUTPUT_STORE_PATH)"
+fi
 
 # Conditionally add --development-mode
 if [ "${PRODUCTION_MODE}" = "true" ]; then
@@ -22,10 +27,10 @@ else
 fi
 
 substreams-sink-files run \
-  "${NETWORK_URL}" \
+  "${SUBSTREAMS_ENDPOINT}" \
   "${MANIFEST}" \
   "${MODULE}" \
-  "${OUTPUT_STORE}" \
+  "${OUTPUT_STORE_PATH}" \
   "${START_BLOCK}:${STOP_BLOCK}" \
   --encoder "${ENCODER}" \
   --parquet-default-column-compression "${PARQUET_DEFAULT_COLUMN_COMPRESSION}" \
